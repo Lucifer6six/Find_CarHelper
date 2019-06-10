c4be4330c03ce88bf732b07453b823fe11bf1501
liluckting@163.com <liting11>
2019-06-10 09:05:35 +0800

    邀请list

---
+++ app/src/main/java/com/find_carhelper/bean/UserBean.java
@@ -0,0 +1,103 @@
+package com.find_carhelper.bean;
+
+public class UserBean {
+    private String code;
+    private String phoneNo;
+    private String nickname;
+    private String companyName;
+    private String realName;
+    private String role;
+    private String status;
+    private String registerTime;
+    private String latestLoginTime;
+    private String latestLoginIp;
+    private String latestLoginDeviceId;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getRealName() {
+        return realName;
+    }
+
+    public void setRealName(String realName) {
+        this.realName = realName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getRegisterTime() {
+        return registerTime;
+    }
+
+    public void setRegisterTime(String registerTime) {
+        this.registerTime = registerTime;
+    }
+
+    public String getLatestLoginTime() {
+        return latestLoginTime;
+    }
+
+    public void setLatestLoginTime(String latestLoginTime) {
+        this.latestLoginTime = latestLoginTime;
+    }
+
+    public String getLatestLoginIp() {
+        return latestLoginIp;
+    }
+
+    public void setLatestLoginIp(String latestLoginIp) {
+        this.latestLoginIp = latestLoginIp;
+    }
+
+    public String getLatestLoginDeviceId() {
+        return latestLoginDeviceId;
+    }
+
+    public void setLatestLoginDeviceId(String latestLoginDeviceId) {
+        this.latestLoginDeviceId = latestLoginDeviceId;
+    }
+}
